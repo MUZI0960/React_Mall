1ba144d91ac1856331d0e53073a505290e9e9fd1
MUZI0960 <giy09060@gmail.com>
2025-04-23 21:42:03 +0900

    4/23

--- src/hooks/useCustomMove.js
+++ src/hooks/useCustomMove.js
@@ -1,55 +1,76 @@
+
 import { useState } from "react"
 import { createSearchParams, useNavigate, useSearchParams } from "react-router-dom"
 
-const getNum = (param, defaultValue) => {
-    if(!param){
-        return defaultValue
-    }
-    return parseInt(param)
+const getNum  = (param, defaultValue) => {
+
+  if(!param){
+    return defaultValue
+  }
+
+  return parseInt(param)
 }
 
 const useCustomMove = () => {
-    
-    const navigate = useNavigate()
 
-    const [refresh, setRefresh] = useState(false)
+  const navigate = useNavigate()
 
-    const [queryParams] = useSearchParams()
+  const [refresh, setRefresh] = useState(false)
 
-    const page = getNum(queryParams.get('page'), 1)
-    const size = getNum(queryParams.get('size'), 10)
+  const [queryParams] = useSearchParams()
+
+  const page = getNum(queryParams.get('page'), 1)
+  const size = getNum(queryParams.get('size'),10)
+
+  const queryDefault = createSearchParams({page, size}).toString() //새로 추가
 
 
-    // page=3&size=10
-    const queryDefault = createSearchParams({page,size}).toString()
+  const moveToList = (pageParam) => {
 
-    const moveToList = (pageParam) => {
-        let queryStr = ""
-        
-        if(pageParam){
-    
-            const pageNum = getNum(pageParam.page, 1)
-            const sizeNum = getNum(pageParam.size, 10)
-    
-            queryStr = createSearchParams({page:pageNum, size: sizeNum}).toString()
+    let queryStr = ""
 
-        }else {
-            queryStr = queryDefault
-        }
+    if(pageParam){
 
-        setRefresh(!refresh)
+      const pageNum = getNum(pageParam.page, 1)
+      const sizeNum = getNum(pageParam.size, 10)
 
-        navigate({pathname:'../list', search: queryStr})
+      queryStr = createSearchParams({page:pageNum, size: sizeNum}).toString()
+    }else {
+      queryStr = queryDefault
     }
 
-    const moveToModify = (num) => {
-        navigate({
-            pathname : `../modify/${num}`,
-            search : queryDefault
-        })
-    }
+    navigate({
+      pathname: `../list`,
+      search:queryStr
+    })
 
-    return {moveToList, moveToModify, page, size, refresh}
+    setRefresh(!refresh) //추가 
+
+
+  }
+
+  const moveToModify = (num) => {
+
+    console.log(queryDefault)
+
+    navigate({
+      pathname: `../modify/${num}`,
+      search: queryDefault  //수정시에 기존의 쿼리 스트링 유지를 위해 
+    })
+  }
+
+  const moveToRead =(num) => {
+
+    console.log(queryDefault)
+
+    navigate({
+      pathname: `../read/${num}`,
+      search: queryDefault
+    })
+  }
+
+
+  return  {moveToList, moveToModify, moveToRead, page, size, refresh} //refresh 추가 
 
 
 }
--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,22 +1,24 @@
-import React from "react";
-import ReadComponent from "../../components/todo/ReadComponent";
 import { useParams } from "react-router-dom";
+import ReadComponent from "../../components/todo/ReadComponent";
 
-function ReadPage(props){
+const ReadPage = () => {
 
-    const {tno} = useParams()
+  const {tno} = useParams()
 
-    return(
-        <div className={'font-extrabold w-full bg-white mt-6'}>
-           
-           <div className="text-2xl">
-                Todo Read Page Component {tno}
-           </div>
 
-            <ReadComponent tno={tno} />
+  return ( 
+    <div className="font-extrabold w-full bg-white mt-6">
 
-        </div>
-    );
+      <div className="text-2xl ">
+        Todo Read Page Component {tno}
+      </div>
+      
+      <ReadComponent tno={tno}></ReadComponent>
+      
+    </div> 
+
+  );
+
 }
-
+ 
 export default ReadPage;
